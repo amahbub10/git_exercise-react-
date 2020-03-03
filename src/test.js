87b706466bc561a13f69ef62c4646603fbb10f23
think <amahbub@think-bricks.com>
2020-03-03 12:36:42 +0600

    atik v2 working

--- src/test.js
+++ src/test.js
@@ -6,7 +6,7 @@ function Test() {
     return (
         <div>
             <p>Hello Rana {count}</p>
-            <h4>Any update?Develop Working</h4>
+            <h4>Any update?Develop Working - Atikv2 working</h4>
             <button onClick={ () => setCount(count + 1)}>Click Me</button>
         </div>
     )
