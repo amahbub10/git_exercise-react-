9bc5243d00eec6fea8c6bfee835fc1f3db577fd2
think <amahbub@think-bricks.com>
2020-03-03 12:07:17 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Test from './test'
+import Test1 from './Test1'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Test/>
+        <Test1/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Test1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Test1() {
+    return (
+        <div>
+            Hello GitHub
+        </div>
+    )
+}
+
+export default Test1
